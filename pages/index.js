adc3331415d1bf2b7727ebea762f79dfc5ef9f4d
NogumaCode <noguma.san.desu@gmail.com>
2022-09-11 10:51:56 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from "next/link";
+import Layout, { siteTitle } from '../components/Layout';
+
+import utilStyles from '../styles/utils.module.css';
+
+import { getPostsData } from '../lib/post';
+
+//SSG
+export async function getStaticProps(){
+  const allPostsData = getPostsData();
+  // console.log(allPostsData);
+  return{
+    props:{
+      allPostsData,
+    },
+  };
+}
+
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Python Develop blog</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2>ブログタイトル</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({id,title,date,thumbnail})=>(
+             <article key ={id}>
+             <Link href ={`/posts/${id}`}>
+             <img src={thumbnail} className={styles.thumbnailImage} />
+             </Link>
+             <Link href ={`/posts/${id}`}>
+               <a className={utilStyles.boldText}>{title}</a>
+             </Link>
+             <br />
+             <small className={utilStyles.lightText}>{date}</small>
+           </article>
+          ))}
+
+        </div>
+      </section>
+    </Layout>
+
+
+  );
+}
